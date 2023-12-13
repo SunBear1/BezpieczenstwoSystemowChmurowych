4c1f0430b82c9cff6aaa208afbd64b880726327d
Łukasz Niedźwiadek <37691734+SunBear1@users.noreply.github.com>
2023-12-13 16:32:42 +0100

    labs finished

--- apps/simple_curl/script.sh
+++ apps/simple_curl/script.sh
@@ -2,7 +2,7 @@
 
 printf "Sending HTTP GET request to: %s ...\n" "${TARGET_ENDPOINT_URL}"
 while true; do
-    curl -sS -X GET "${TARGET_ENDPOINT_URL}"
+    curl --max-time 10 -sS -X GET "${TARGET_ENDPOINT_URL}"
     printf "\n"
     sleep 5
 done
